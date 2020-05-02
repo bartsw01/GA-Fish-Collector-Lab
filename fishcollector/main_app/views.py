5770fd815f6b498a905db38c5e70f3872daae193
Bart Webber <bartacus.webber@gmail.com>
2020-05-02 15:29:55 -0700

    added fish detail page

--- fishcollector/main_app/views.py
+++ fishcollector/main_app/views.py
@@ -22,8 +22,12 @@ def home(request):
   return render(request, 'home.html')
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 
 def fishes_index(request):
   fishes = Fish.objects.all()
-  return render(request, 'fishes/index.html', {'fishes': fishes})    
+  return render(request, 'fishes/index.html', {'fishes': fishes})   
+
+def fishes_detail(request, fish_id):
+  fish = Fish.objects.get(id=fish_id)
+  return render(request, 'fishes/detail.html', { 'fish': fish})   
