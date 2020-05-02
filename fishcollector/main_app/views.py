8dc9a9b6175d60326c3d52a8fa169e99bf291e0e
Bart Webber <bartacus.webber@gmail.com>
2020-05-02 14:30:24 -0700

    connected to fishcollector database

--- fishcollector/main_app/views.py
+++ fishcollector/main_app/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
+from .models import Fish
 
 # Create your views here.
 from django.http import HttpResponse
 
 # Add the Cat class & list and view function below the imports
-class Fish:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, size, description, age):
-    self.name = name
-    self.size = size
-    self.description = description
-    self.age = age
+# class Fish:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, size, description, age):
+#     self.name = name
+#     self.size = size
+#     self.description = description
+#     self.age = age
 
-fishes = [
-  Fish('Tilapia', '2 lbs', 'freshwater food fish', 3),
-  Fish('Blue Gill', '3-4 lbs', 'deep blue purple on face', 2),
-  Fish('Trout', '10 lbs', 'spotted rainbow trout', 4)
-]
+# fishes = [
+#   Fish('Tilapia', '2 lbs', 'freshwater food fish', 3),
+#   Fish('Blue Gill', '3-4 lbs', 'deep blue purple on face', 2),
+#   Fish('Trout', '10 lbs', 'spotted rainbow trout', 4)
+# ]
 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def fishes_index(request):
-    return render(request, 'fishes/index.html', {'fishes': fishes})    
+  fishes = Fish.objects.all()
+  return render(request, 'fishes/index.html', {'fishes': fishes})    
